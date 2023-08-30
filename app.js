e6a6943a1ae85e345a91c4598219b4bc87b49142
Krishrx <kalikrishnas342@gmail.com>
2023-08-30 19:27:44 +0530

    completed the projects section

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ menuBtn.addEventListener('click',()=>{
     navBar.classList.toggle('hidden');
     menuBtn.classList.toggle('fa-xmark');
 })
+
+const projDivs = document.querySelectorAll('.projDivs');
+
+projDivs.forEach(projDiv=>{
+    projDiv.addEventListener('mouseenter',()=>{
+        projDiv.lastElementChild.classList.toggle('hidden');
+    })
+    projDiv.addEventListener('mouseleave',()=>{
+        projDiv.lastElementChild.classList.toggle('hidden');
+    })
+})
