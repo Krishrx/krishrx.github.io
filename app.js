cd4f84cb9ec62ed32054c9fbde64fc8e46276b2b
Krishrx <kalikrishnas342@gmail.com>
2023-08-30 21:22:27 +0530

    completed contact me & footer

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ projDivs.forEach(projDiv=>{
         projDiv.lastElementChild.classList.toggle('hidden');
     })
 })
+
+openModalButton.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+  });
+
+  closeModalButton.addEventListener('click', () => {
+      modal.classList.add('hidden');
+  });
+  
+  
+  
+  window.addEventListener('click',(e)=>{
+    if(e.target===modal){
+      modal.classList.add('hidden');
+    }
+  })
